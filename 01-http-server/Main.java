9cd7367ede2fe5d08120e3df31a17be26fd036b0
Krishna <kriishna002@gmail.com>
2026-09-08 02:06:10 +0530

    Send HTTP response

--- 01-http-server/Main.java
+++ 01-http-server/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("HTTP Server Project");
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        ServerSocket  serverSocket  =new ServerSocket(8080);
+        System.out.println("Server stated on port 8080");
+
+        var clientSocket = serverSocket.accept();
+        System.out.println("server connected");
+
+        var output = clientSocket.getOutputStream();
+
+        // output.write("Hello java server !".getBytes());
+        output.write("HTTP/1.1 200 OK\r\n\r\nHello java server!".getBytes());
+        output.flush(); 
+        clientSocket.close();
     }
 }
